0e4e267de0683360c282ccb9a98722ae2a8b864d
Darryl Kilzer <darrylkilzer@gmail.com>
2018-11-27 13:57:18 -0700

    syntax highlighting working

--- src/main.js
+++ src/main.js
@@ -2,6 +2,11 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import VuePrism from 'vue-prism'
+
+Vue.use(VuePrism)
+
+import 'prismjs/themes/prism.css'
 
 Vue.config.productionTip = false
 
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,12 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <pre class="language-js">
+      <code>
+        export default {
+          test: TEST
+        }
+      </code>
+    </pre>
   </div>
 </template>
